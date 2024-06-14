f6631fe7a7ad4643cbced627def0d1c11aa3cc48
Koongh <yapkng@gmail.com>
2024-06-15 00:42:46 +0700

    fix: styling crm page

--- app/src/app/crm/page.tsx
+++ app/src/app/crm/page.tsx
@@ -1,22 +1,19 @@
-"use server"
+"use server";
 
-import {Payment, columns} from "./column"
-import {DataTable} from "./data-table"
-import {getAllCustomer} from "@/services/customer-action";
-
+import { Payment, columns } from "./column";
+import { DataTable } from "./data-table";
+import { getAllCustomer } from "@/services/customer-action";
 
 const AnalyticsPage = async () => {
-  const customers = await getAllCustomer()
+  const customers = await getAllCustomer();
 
   return (
-      <section
-          className={"flex flex-col min-h-screen py-2"}
-      >
-        <div className="container mx-auto py-10">
-          <DataTable columns={columns} data={customers}/>
-        </div>
-      </section>
+    <section className={"flex flex-col min-h-screen py-2"}>
+      <div className="container mx-auto py-10 ">
+        <DataTable columns={columns} data={customers} />
+      </div>
+    </section>
   );
-}
+};
 
 export default AnalyticsPage;
