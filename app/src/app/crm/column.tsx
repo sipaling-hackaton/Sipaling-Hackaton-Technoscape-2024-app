9eb22b68b725858fcf4509ef67ae308f7b03cf42
Aurelius Ivan Wijaya <aureliusivanwijaya@gmail.com>
2024-06-15 00:25:28 +0700

    feature/CRM-filter-data

--- app/src/app/crm/column.tsx
+++ app/src/app/crm/column.tsx
@@ -23,11 +23,31 @@ export type Payment = {
 export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "id",
-    header: "ID",
+    header: ({column}) => {
+      return (
+          <Button
+              variant="ghost"
+              onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            ID
+            <ArrowUpDown className="ml-2 h-4 w-4"/>
+          </Button>
+      )
+    }
   },
   {
     accessorKey: "name",
-    header: "Name",
+    header: ({column}) => {
+      return (
+          <Button
+              variant="ghost"
+              onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Name
+            <ArrowUpDown className="ml-2 h-4 w-4"/>
+          </Button>
+      )
+    }
   },
   {
     accessorKey: "email",
@@ -45,7 +65,17 @@ export const columns: ColumnDef<Payment>[] = [
   },
   {
     accessorKey: "phone",
-    header: "Phone",
+    header: ({column}) => {
+      return (
+          <Button
+              variant="ghost"
+              onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Phone
+            <ArrowUpDown className="ml-2 h-4 w-4"/>
+          </Button>
+      )
+    }
   },
   {
     accessorKey: "address",
@@ -66,14 +96,24 @@ export const columns: ColumnDef<Payment>[] = [
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
               <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem
-                  onClick={() => navigator.clipboard.writeText(payment.id)}
-              >
-                Copy payment ID
-              </DropdownMenuItem>
+              {/*<DropdownMenuItem*/}
+              {/*    onClick={() => navigator.clipboard.writeText(payment.id)}*/}
+              {/*>*/}
+              {/*  Copy payment ID*/}
+              {/*</DropdownMenuItem>*/}
               <DropdownMenuSeparator/>
-              <DropdownMenuItem>View customer</DropdownMenuItem>
-              <DropdownMenuItem>View payment details</DropdownMenuItem>
+              <DropdownMenuItem
+                  className={"bg-gradient-to-r from-[#7a2180] to-[#e40276] text-white rounded-lg hover:opacity-50"}
+                  onClick={(e: any) => {
+                    e.preventDefault()
+                    // prevent propagation
+                    e.stopPropagation()
+
+                    window.location.href = `/${payment.id}`
+                  }}
+              >
+                AI Chat
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
       )
